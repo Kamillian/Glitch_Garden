884a5438870a383bdff31de3251d2cee15b28200
Alexey Demidov <kamillian@bk.ru>
2020-10-03 09:38:11 +0300

    added some scene

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+    [SerializeField] int timeToDelay = 3;
+
+    private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            LoadNextScene();
+        }
+    }
+
+
+    public void LoadNextScene()
+    {
+        StartCoroutine(DelayBelowLoad());
+    }
+
+    IEnumerator DelayBelowLoad()
+    {
+        yield return new WaitForSeconds(timeToDelay);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+}
