5907729098313de0bfc22dd05346fd1976afe93e
Alexey Demidov <kamillian@bk.ru>
2021-01-11 15:55:03 +0300

    added spawner

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+    [SerializeField] Attacked attackerPrefab;
+
+    bool spanw = true;
+
+    IEnumerator Start()
+    {
+        while (spanw)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
